8656cdc83dbf3d74e2cace6d74bd0a93a31881a1
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-12-20 00:56:09 +0100

    styled the footer component

--- netflixwebapp/src/components/Faq.jsx
+++ netflixwebapp/src/components/Faq.jsx
@@ -3,147 +3,156 @@ import OpenIcon from './OpenIcon';
 
 export default function Faq() {
   return (
-    <div className='w-full max-w-[750px] m-auto p-3 text-center text-sm text-white pb-16'>
-      <h2 className='text-white/80 font-semibold tracking-wider text-3xl'>
-        Frequently Asked Questions
-      </h2>
-      <ul className='mt-8'>
-        <li className='w-full p-1.5'>
-          <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] cursor-pointer'>
-            {' '}
-            <input className='hidden peer' type='radio' name='accordion' />
-            <div className='w-full flex justify-between items-center'>
-              <p className=''>What is Netflix?</p>
-              <OpenIcon />
-            </div>
-            <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
-              <p>
-                Netflix is a streaming service that offers a wide variety of
-                award-winning TV shows, movies, anime, documentaries, and more
-                on thousands of internet-connected devices.
-              </p>
-              <br />
-              <p>
-                You can watch as much as you want, whenever you want without a
-                single commercial – all for one low monthly price. There's
-                always something new to discover and new TV shows and movies are
-                added every week!
-              </p>
-            </div>
-          </label>
-        </li>
-        <li className='w-full p-1.5'>
-          <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] mb-0.5 cursor-pointer '>
-            {' '}
-            <input className='hidden peer' type='radio' name='accordion' />
-            <div className='w-full flex justify-between items-center'>
-              <p className=''>How much does Netflix cost?</p>
-              <OpenIcon />
-            </div>
-            <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
-              <p>
-                Watch Netflix on your smartphone, tablet, Smart TV, laptop, or
-                streaming device, all for one fixed monthly fee. Plans range
-                from EUR7.99 to EUR11.99 a month. No extra costs, no contracts.
-              </p>
-            </div>
-          </label>
-        </li>
-        <li className='w-full p-1.5'>
-          <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] mb-0.5 cursor-pointer '>
-            {' '}
-            <input className='hidden peer' type='radio' name='accordion' />
-            <div className='w-full flex justify-between items-center'>
-              <p className=''>Where can I watch?</p>
-              <OpenIcon />
-            </div>
-            <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
-              <p>
-                Watch anywhere, anytime. Sign in with your Netflix account to
-                watch instantly on the web at netflix.com from your personal
-                computer or on any internet-connected device that offers the
-                Netflix app, including smart TVs, smartphones, tablets,
-                streaming media players and game consoles.
-              </p>
-              <p>
-                You can also download your favorite shows with the iOS, Android,
-                or Windows 10 app. Use downloads to watch while you're on the go
-                and without an internet connection. Take Netflix with you
-                anywhere.
-              </p>
-            </div>
-          </label>
-        </li>
-        <li className='w-full p-1.5'>
-          <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] mb-0.5 cursor-pointer '>
-            {' '}
-            <input className='hidden peer' type='radio' name='accordion' />
-            <div className='w-full flex justify-between items-center'>
-              <p className=''>How do I cancel?</p>
-              <OpenIcon />
-            </div>
-            <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
-              <p>
-                Netflix is flexible. There are no pesky contracts and no
-                commitments. You can easily cancel your account online in two
-                clicks. There are no cancellation fees – start or stop your
-                account anytime.
-              </p>
-            </div>
-          </label>
-        </li>
-        <li className='w-full p-1.5'>
-          <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] mb-0.5 cursor-pointer '>
-            {' '}
-            <input className='hidden peer' type='radio' name='accordion' />
-            <div className='w-full flex justify-between items-center'>
-              <p className=''>What can I watch on Netflix?</p>
-              <OpenIcon />
-            </div>
-            <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
-              <p>
-                Netflix has an extensive library of feature films,
-                documentaries, TV shows, anime, award-winning Netflix originals,
-                and more. Watch as much as you want, anytime you want.
-              </p>
-            </div>
-          </label>
-        </li>
-        <li className='w-full p-1.5'>
-          <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] mb-0.5 cursor-pointer '>
-            {' '}
-            <input className='hidden peer' type='radio' name='accordion' />
-            <div className='w-full flex justify-between items-center'>
-              <p className=''>Is Netflix good for kids?</p>
-              <OpenIcon />
-            </div>
-            <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
-              <p>
-                Netflix has an extensive library of feature films,
-                documentaries, TV shows, anime, award-winning Netflix originals,
-                and more. Watch as much as you want, anytime you want.
-              </p>
-            </div>
-          </label>
-        </li>{' '}
-      </ul>
-      <small>
-        Ready to watch? Enter your email to create or restart your membership.
-      </small>
-      <form className='w-full m-auto max-w-xl bg-white text-center rounded-md flex items-center outline-none mt-7 overflow-hidden'>
-        <input
-          className='flex-1 outline-none ml-5 text-slate-700'
-          type='email'
-          placeholder='Email address'
-          required
-        />
-        <button
-          className='bg-[#db0001]  text-white text-sm px-8 py-3 cursor-pointer outline-none '
-          type='submit'
-        >
-          Get Started
-        </button>
-      </form>
-    </div>
+    <>
+      <div className='border-b-8 border-b-[#303030]'>
+        <div className='w-full max-w-[750px] m-auto  text-center text-sm text-white pb-16 '>
+          <h2 className='text-white/80 font-semibold tracking-wider text-3xl'>
+            Frequently Asked Questions
+          </h2>
+          <ul className='mt-8'>
+            <li className='w-full p-1.5'>
+              <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] cursor-pointer'>
+                {' '}
+                <input className='hidden peer' type='radio' name='accordion' />
+                <div className='w-full flex justify-between items-center'>
+                  <p className=''>What is Netflix?</p>
+                  <OpenIcon />
+                </div>
+                <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
+                  <p>
+                    Netflix is a streaming service that offers a wide variety of
+                    award-winning TV shows, movies, anime, documentaries, and
+                    more on thousands of internet-connected devices.
+                  </p>
+                  <br />
+                  <p>
+                    You can watch as much as you want, whenever you want without
+                    a single commercial – all for one low monthly price. There's
+                    always something new to discover and new TV shows and movies
+                    are added every week!
+                  </p>
+                </div>
+              </label>
+            </li>
+            <li className='w-full p-1.5'>
+              <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] mb-0.5 cursor-pointer '>
+                {' '}
+                <input className='hidden peer' type='radio' name='accordion' />
+                <div className='w-full flex justify-between items-center'>
+                  <p className=''>How much does Netflix cost?</p>
+                  <OpenIcon />
+                </div>
+                <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
+                  <p>
+                    Watch Netflix on your smartphone, tablet, Smart TV, laptop,
+                    or streaming device, all for one fixed monthly fee. Plans
+                    range from EUR7.99 to EUR11.99 a month. No extra costs, no
+                    contracts.
+                  </p>
+                </div>
+              </label>
+            </li>
+            <li className='w-full p-1.5'>
+              <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] mb-0.5 cursor-pointer '>
+                {' '}
+                <input className='hidden peer' type='radio' name='accordion' />
+                <div className='w-full flex justify-between items-center'>
+                  <p className=''>Where can I watch?</p>
+                  <OpenIcon />
+                </div>
+                <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
+                  <p>
+                    Watch anywhere, anytime. Sign in with your Netflix account
+                    to watch instantly on the web at netflix.com from your
+                    personal computer or on any internet-connected device that
+                    offers the Netflix app, including smart TVs, smartphones,
+                    tablets, streaming media players and game consoles.
+                  </p>
+                  <br />
+                  <p>
+                    You can also download your favorite shows with the iOS,
+                    Android, or Windows 10 app. Use downloads to watch while
+                    you're on the go and without an internet connection. Take
+                    Netflix with you anywhere.
+                  </p>
+                </div>
+              </label>
+            </li>
+            <li className='w-full p-1.5'>
+              <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] mb-0.5 cursor-pointer '>
+                {' '}
+                <input className='hidden peer' type='radio' name='accordion' />
+                <div className='w-full flex justify-between items-center'>
+                  <p className=''>How do I cancel?</p>
+                  <OpenIcon />
+                </div>
+                <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
+                  <p>
+                    Netflix is flexible. There are no pesky contracts and no
+                    commitments. You can easily cancel your account online in
+                    two clicks. There are no cancellation fees – start or stop
+                    your account anytime.
+                  </p>
+                </div>
+              </label>
+            </li>
+            <li className='w-full p-1.5'>
+              <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] mb-0.5 cursor-pointer '>
+                {' '}
+                <input className='hidden peer' type='radio' name='accordion' />
+                <div className='w-full flex justify-between items-center'>
+                  <p className=''>What can I watch on Netflix?</p>
+                  <OpenIcon />
+                </div>
+                <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
+                  <p>
+                    Netflix has an extensive library of feature films,
+                    documentaries, TV shows, anime, award-winning Netflix
+                    originals, and more. Watch as much as you want, anytime you
+                    want.
+                  </p>
+                </div>
+              </label>
+            </li>
+            <li className='w-full p-1.5'>
+              <label className='peer relative flex flex-col  items-center justify-between p-4 text-sm font-medium bg-[#303030] mb-0.5 cursor-pointer '>
+                {' '}
+                <input className='hidden peer' type='radio' name='accordion' />
+                <div className='w-full flex justify-between items-center'>
+                  <p className=''>Is Netflix good for kids?</p>
+                  <OpenIcon />
+                </div>
+                <div className='bg-[#303030] text-left px-5 overflow-hidden max-h-0 peer-checked:max-h-screen peer-checked:mt-4'>
+                  <p>
+                    Netflix has an extensive library of feature films,
+                    documentaries, TV shows, anime, award-winning Netflix
+                    originals, and more. Watch as much as you want, anytime you
+                    want.
+                  </p>
+                </div>
+              </label>
+            </li>{' '}
+          </ul>
+          <small>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </small>
+          <form className='w-full m-auto max-w-xl bg-white text-center rounded-md flex items-center outline-none mt-7 overflow-hidden'>
+            <input
+              className='flex-1 outline-none ml-5 text-slate-700'
+              type='email'
+              placeholder='Email address'
+              required
+            />
+            <button
+              className='bg-[#db0001]  text-white text-sm px-8 py-3 cursor-pointer outline-none '
+              type='submit'
+            >
+              Get Started
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
--- netflixwebapp/src/components/Footer.jsx
+++ netflixwebapp/src/components/Footer.jsx
@@ -2,33 +2,65 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className='text-white pt-12 px-[15%] pb-3 border-t-8 border-t-[#303030]'>
+    <div className='max-w-3xl pt-12 px-10  m-auto pb-3  text-[#777]'>
       <div className=''>
-        <h2>Questions? call 000-111-222-333</h2>
+        <h2 className='text-lg font-normal mb-7'>
+          Questions? call 000-111-222-333
+        </h2>
       </div>
-      <div>
+      <div className='flex justify-between mb-5 gap-4'>
         <div>
-          <a href='#'>FAQ</a>
-          <a href='#'>Investor Relations</a>
-          <a href='#'>Privacy</a>
-          <a href='#'>Speed Test</a>
+          <a className='block text-sm mb-2.5' href='#'>
+            FAQ
+          </a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Investor Relations
+          </a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Privacy
+          </a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Speed Test
+          </a>
         </div>
         <div>
-          <a href='#'>Help Center</a>
-          <a href='#'>Jobs</a>
-          <a href='#'>Cookies Preferences</a>
-          <a href='#'>Legal Notices</a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Help Center
+          </a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Jobs
+          </a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Cookies Preferences
+          </a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Legal Notices
+          </a>
         </div>
         <div>
-          <a href='#'>Account</a>
-          <a href='#'>Ways to watch</a>
-          <a href='#'>Corporate Information</a>
-          <a href='#'>Only on Netflix</a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Account
+          </a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Ways to watch
+          </a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Corporate Information
+          </a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Only on Netflix
+          </a>
         </div>
         <div>
-          <a href='#'>Media Centre</a>
-          <a href='#'>Terms of Use</a>
-          <a href='#'>Contact Us</a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Media Centre
+          </a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Terms of Use
+          </a>
+          <a className='block text-sm mb-2.5' href='#'>
+            Contact Us
+          </a>
         </div>
       </div>
     </div>
