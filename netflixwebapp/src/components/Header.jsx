a827a584399a9a58007112edfcaf3d51fcdfa918
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-12-21 23:21:07 +0100

    finished this project, i will come back again to  mantain it ...

--- netflixwebapp/src/components/Header.jsx
+++ netflixwebapp/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { ChevronRight } from './OpenIcon';
 
 export default function Header() {
   return (
-    <div className='w-full  border-b-8 border-b-[#222] h-screen py-4 px-3 bg-hero bg-[0,0,0,.4] bg-cover bg-center'>
-      <nav className='flex items-center justify-between py-2.5'>
-        <img className='w-36 cursor-pointer' src='logo.png' alt='' />
+    <div className='w-full border-b-8 border-b-[#222] h-screen py-4 px-3 bg-hero bg-cover bg-center'>
+      <nav className='flex items-center justify-between 2xl:px-10 px-7 py-2.5'>
+        <img
+          className='w-28 lg:w-36 2xl:w-40 cursor-pointer'
+          src='logo.png'
+          alt=''
+        />
         <div>
           <button className='outline-none bg-[#db0001] text-white text-xs rounded-md px-3 py-1.5 ml-2 cursor-pointer inline-flex items-center bg-transparent border'>
             English <img className='w-2 ml-2 ' src='down-icon.png' alt='' />
@@ -14,29 +19,30 @@ export default function Header() {
           </button>
         </div>
       </nav>
-      <div className='w-full text-white flex flex-col justify-center items-center h-screen '>
-        <h1 className='text-6xl font-semibold max-w-2xl text-center mb-4'>
+      <div className='text-white flex flex-col justify-center items-center h-screen max-w-md sm:max-w-3xl m-auto'>
+        <h1 className='sm:text-5xl lg:text-6xl text-4xl font-bold max-w-2xl -mt-28 text-center mb-4 tracking-wide'>
           Unlimited movies, TV shows and more.
         </h1>
-        <h3 className='font-semibold text-3xl mb-5'>
+        <h3 className='font-semibold sm:text-2xl text-xl md:text-3xl mb-5'>
           Watch anywhere. Cancel anytime
         </h3>
-        <p className='text-lg font-normal'>
+        <p className='text-xl md:text-lg text-center font-semibold'>
           Ready to watch? Enter your email to create or restrart your
           membership.
         </p>
-        <form className='w-full max-w-xl bg-white text-center rounded-md flex items-center outline-none mt-7 overflow-hidden'>
+        <form className='w-full md:max-w-2xl bg-white text-center rounded-md flex items-center outline-none mt-3 overflow-hidden'>
           <input
-            className='flex-1 outline-none ml-5 text-slate-700'
+            className='flex-1 outline-none ml-5  text-slate-700'
             type='email'
             placeholder='Email address'
             required
           />
           <button
-            className='bg-[#db0001]  text-white text-sm px-8 py-3 cursor-pointer outline-none '
+            className='flex md:text-xl text-lg items-center font-semibold bg-[#db0001]  text-white gap-1 px-2 py-4 lg:py-5 lg:px-8 cursor-pointer outline-none '
             type='submit'
           >
             Get Started
+            <ChevronRight />
           </button>
         </form>
       </div>
--- netflixwebapp/src/components/OpenIcon.jsx
+++ netflixwebapp/src/components/OpenIcon.jsx
@@ -21,3 +21,11 @@ export default function OpenIcon() {
     </div>
   );
 }
+export function ChevronRight() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+</svg>
+
+  )
+}
